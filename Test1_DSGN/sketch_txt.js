3a881a80b777ec89613c3295ac244809ef1d0f50
Pabecy <101276075+Pabecy@users.noreply.github.com>
2022-05-10 17:17:48 -0400

    Cambiar Puerto

--- Test1_DSGN/sketch_txt.js
+++ Test1_DSGN/sketch_txt.js
@@ -10,7 +10,7 @@ let serial;
 
 // declarar variable con nombre de puerto de Arduino
 // actualizar con nombre del puerto en tu computador
-let nombrePuerto = "COM5";
+let nombrePuerto = "COM3";
 
 // declarar variable para datos recibidos
 let datosRecibidos = "esperando...";
